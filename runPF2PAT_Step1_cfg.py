eadd1ea6cad1618e2c51deb113f568d520aee661
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2014-04-03 01:14:01 -0500

    initial commit

---
+++ runPF2PAT_Step1_cfg.py
@@ -0,0 +1,232 @@
+###############################
+####### Parameters ############
+###############################
+from FWCore.ParameterSet.VarParsing import VarParsing
+import string
+
+options = VarParsing ('python')
+
+options.register('runOnData', False,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Run this on real data"
+)
+## Make sure correct global tags are used (please refer to https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions)
+options.register('mcGlobalTag', 'START53_V27',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "MC global tag"
+)
+options.register('dataGlobalTag', 'FT53_V21A_AN6',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "Data global tag"
+)
+options.register('outFilename', 'patTuple_PF2PAT',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "Output file name"
+)
+options.register('reportEvery', 10,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.int,
+    "Report every N events (default is N=10)"
+)
+options.register('wantSummary', False,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Print out trigger and timing summary"
+)
+options.register('usePFchs', True,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Use PFchs"
+)
+
+## 'maxEvents' is already registered by the Framework, changing default value
+options.setDefault('maxEvents', 100)
+
+options.parseArguments()
+
+print "Running on data: %s"%('True' if options.runOnData else 'False')
+print "Using PFchs: %s"%('True' if options.usePFchs else 'False')
+
+## Global tag
+globalTag = options.mcGlobalTag
+if options.runOnData:
+    globalTag = options.dataGlobalTag
+
+## Jet energy corrections
+inputJetCorrLabelAK5 = ('AK5PFchs', ['L1FastJet', 'L2Relative', 'L3Absolute'])
+inputJetCorrLabelAK7 = ('AK7PFchs', ['L1FastJet', 'L2Relative', 'L3Absolute'])
+
+if not options.usePFchs:
+    inputJetCorrLabelAK5 = ('AK5PF', ['L1FastJet', 'L2Relative', 'L3Absolute'])
+    inputJetCorrLabelAK7 = ('AK7PF', ['L1FastJet', 'L2Relative', 'L3Absolute'])
+
+if options.runOnData:
+    inputJetCorrLabelAK5[1].append('L2L3Residual')
+    inputJetCorrLabelAK7[1].append('L2L3Residual')
+
+## b tagging
+bTagInfos = ['impactParameterTagInfos','secondaryVertexTagInfos']
+             #,'inclusiveSecondaryVertexFinderTagInfos','inclusiveSecondaryVertexFinderFilteredTagInfos','softMuonTagInfos','softElectronTagInfos']
+bTagDiscriminators = ['jetProbabilityBJetTags','combinedSecondaryVertexBJetTags']
+                      #,'trackCountingHighPurBJetTags','trackCountingHighEffBJetTags','jetBProbabilityBJetTags'
+                      #,'simpleSecondaryVertexHighPurBJetTags','simpleSecondaryVertexHighEffBJetTags'
+                      #,'simpleInclusiveSecondaryVertexHighEffBJetTags','simpleInclusiveSecondaryVertexHighPurBJetTags',
+                      #,'combinedInclusiveSecondaryVertexBJetTags','doubleSecondaryVertexHighEffBJetTags']
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PF2PAT")
+
+## MessageLogger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+############## IMPORTANT ########################################
+# If you run over many samples and you save the log, remember to reduce
+# the size of the output by prescaling the report of the event number
+process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
+process.MessageLogger.cerr.default.limit = 10
+#################################################################
+
+## Geometry and Detector Conditions
+process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = globalTag + '::All'
+
+## Events to process
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+
+## Options and Output Report
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(options.wantSummary) )
+
+#-------------------------------------
+## Input files
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        # /TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7C-v1/AODSIM
+        '/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7C-v1/00000/008BD264-1526-E211-897A-00266CFFA7BC.root'
+    )
+)
+
+if options.runOnData:
+    process.source.fileNames = [
+        # /SingleMu/Run2012D-22Jan2013-v1/AOD
+        '/store/data/Run2012D/SingleMu/AOD/22Jan2013-v1/10000/0449388F-D2A7-E211-BEED-E0CB4E55363D.root'
+    ]
+
+#-------------------------------------
+## Standard PAT Configuration File
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+## Configure PAT to use PF2PAT instead of AOD sources
+## this function will modify the PAT sequences.
+from PhysicsTools.PatAlgos.tools.pfTools import *
+
+outFilename = string.replace(options.outFilename,'.root','') + '_mc.root'
+if options.runOnData :
+    outFilename = string.replace(options.outFilename,'.root','') + '_data.root'
+
+## Output Module Configuration (expects a path 'p')
+from PhysicsTools.PatAlgos.patEventContent_cff import patEventContent
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string(outFilename),
+    # save only events passing the full path
+    SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
+    # save PAT Layer 1 output; you need a '*' to
+    # unpack the list of commands 'patEventContent'
+    outputCommands = cms.untracked.vstring('drop *', *patEventContent)
+)
+
+postfix = "PFlow"
+jetAlgo="AK5"
+usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=not options.runOnData, postfix=postfix,
+          jetCorrections=inputJetCorrLabelAK5, pvCollection=cms.InputTag('goodOfflinePrimaryVertices'))
+
+## Top projections in PF2PAT
+getattr(process,"pfPileUp"+postfix).checkClosestZVertex = False
+getattr(process,"pfNoPileUp"+postfix).enable = options.usePFchs
+getattr(process,"pfNoMuon"+postfix).enable = True
+getattr(process,"pfNoElectron"+postfix).enable = True
+getattr(process,"pfNoTau"+postfix).enable = False
+getattr(process,"pfNoJet"+postfix).enable = True
+
+#-------------------------------------
+from PhysicsTools.PatAlgos.tools.coreTools import *
+## Remove taus from the PAT sequence
+removeSpecificPATObjects(process,names=['Taus'],postfix=postfix)
+
+## Remove MC matching when running over data
+if options.runOnData:
+    removeMCMatching( process, ['All'] )
+
+#-------------------------------------
+## Produce a collection of good primary vertices
+from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
+process.goodOfflinePrimaryVertices = cms.EDFilter("PrimaryVertexObjectFilter",
+    filterParams = pvSelector.clone(
+        minNdof = cms.double(4.0), # this is >= 4
+        maxZ = cms.double(24.0),
+        maxRho = cms.double(2.0)
+    ),
+    src = cms.InputTag('offlinePrimaryVertices')
+)
+
+## Good primary vertex event filter
+process.primaryVertexFilter = cms.EDFilter('VertexSelector',
+    src = cms.InputTag('offlinePrimaryVertices'),
+    cut = cms.string('!isFake & ndof > 4 & abs(z) <= 24 & position.Rho <= 2'),
+    filter = cms.bool(True)
+)
+
+#-------------------------------------
+## Tweak output event content
+from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoCleaning
+patEventContentNoCleaning.append('drop *_selectedPatPFParticlesPFlow_*_*')
+patEventContentNoCleaning.append('drop *_selectedPatJetsPFlow_*_*')
+patEventContentNoCleaning.append('keep GenEventInfoProduct_generator_*_*')
+patEventContentNoCleaning.append('keep PileupSummaryInfos_*_*_*')
+## GenParticles
+patEventContentNoCleaning.append('keep recoGenParticles_genParticles_*_*')
+## For PF jets
+patEventContentNoCleaning.append('keep *_pfNoElectronPFlow_*_*')
+patEventContentNoCleaning.append('keep *_kt6PFJets_rho_*')
+## HLT trigger results
+patEventContentNoCleaning.append('drop *_TriggerResults_*_*')
+patEventContentNoCleaning.append('keep *_TriggerResults_*_HLT')
+## For b tagging
+patEventContentNoCleaning.append('keep *_offlineBeamSpot_*_*')
+patEventContentNoCleaning.append('keep *_generalTracks_*_*')
+patEventContentNoCleaning.append('keep *_offlinePrimaryVertices_*_*')
+## For soft lepton taggers
+#patEventContentNoCleaning.append('keep recoMuons_muons_*_*')
+#patEventContentNoCleaning.append('keep recoTracks_globalMuons_*_*')
+#patEventContentNoCleaning.append('keep *_gsfElectrons_*_*')
+#patEventContentNoCleaning.append('keep *_gsfElectronCores_*_*')
+#patEventContentNoCleaning.append('keep *_electronGsfTracks_*_*')
+
+process.out.outputCommands = cms.untracked.vstring('drop *', *patEventContentNoCleaning)
+
+#-------------------------------------
+## Remove tau stuff that really shouldn't be there (probably a bug in PAT)
+process.patDefaultSequencePFlow.remove(process.kt6PFJetsForRhoComputationVoronoiPFlow)
+for m in getattr(process,"patDefaultSequence"+postfix).moduleNames():
+    if m.startswith('hpsPFTau'):
+        getattr(process,"patDefaultSequence"+postfix).remove(getattr(process,m))
+
+#-------------------------------------
+## Path definition
+process.p = cms.Path(
+    process.primaryVertexFilter
+    * process.goodOfflinePrimaryVertices
+    * getattr(process,"patPF2PATSequence"+postfix)
+)
+
+## Define Endpath
+process.outpath = cms.EndPath(process.out)
+
+## Schedule definition
+process.schedule = cms.Schedule(process.p)
+process.schedule.append(process.outpath)
---
+++ runPF2PAT_Step2_cfg.py
@@ -0,0 +1,375 @@
+###############################
+####### Parameters ############
+###############################
+from FWCore.ParameterSet.VarParsing import VarParsing
+import string
+
+options = VarParsing ('python')
+
+options.register('runOnData', False,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Run this on real data"
+)
+## Make sure correct global tags are used (please refer to https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions)
+options.register('mcGlobalTag', 'START53_V27',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "MC global tag"
+)
+options.register('dataGlobalTag', 'FT53_V21A_AN6',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "Data global tag"
+)
+options.register('outFilename', 'outfile',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "Output file name"
+)
+options.register('reportEvery', 10,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.int,
+    "Report every N events (default is N=10)"
+)
+options.register('wantSummary', False,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Print out trigger and timing summary"
+)
+options.register('usePFchs', True,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Use PFchs"
+)
+options.register('doJTA', True,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Run jet-track association"
+)
+options.register('useExplicitJTA', False,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Use explicit jet-track association"
+)
+options.register('doBTagging', True,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Run b tagging"
+)
+## 'maxEvents' is already registered by the Framework, changing default value
+options.setDefault('maxEvents', 100)
+
+options.parseArguments()
+
+print "Running on data: %s"%('True' if options.runOnData else 'False')
+print "Using PFchs: %s"%('True' if options.usePFchs else 'False')
+
+## Global tag
+globalTag = options.mcGlobalTag
+if options.runOnData:
+    globalTag = options.dataGlobalTag
+
+## Jet energy corrections
+inputJetCorrLabelAK5 = ('AK5PFchs', ['L1FastJet', 'L2Relative', 'L3Absolute'])
+inputJetCorrLabelAK7 = ('AK7PFchs', ['L1FastJet', 'L2Relative', 'L3Absolute'])
+
+if not options.usePFchs:
+    inputJetCorrLabelAK5 = ('AK5PF', ['L1FastJet', 'L2Relative', 'L3Absolute'])
+    inputJetCorrLabelAK7 = ('AK7PF', ['L1FastJet', 'L2Relative', 'L3Absolute'])
+
+if options.runOnData:
+    inputJetCorrLabelAK5[1].append('L2L3Residual')
+    inputJetCorrLabelAK7[1].append('L2L3Residual')
+
+## b tagging
+bTagInfos = ['impactParameterTagInfos','secondaryVertexTagInfos']
+             #,'inclusiveSecondaryVertexFinderTagInfos','inclusiveSecondaryVertexFinderFilteredTagInfos','softMuonTagInfos','softElectronTagInfos']
+bTagDiscriminators = ['jetProbabilityBJetTags','combinedSecondaryVertexBJetTags']
+                      #,'trackCountingHighPurBJetTags','trackCountingHighEffBJetTags','jetBProbabilityBJetTags'
+                      #,'simpleSecondaryVertexHighPurBJetTags','simpleSecondaryVertexHighEffBJetTags'
+                      #,'simpleInclusiveSecondaryVertexHighEffBJetTags','simpleInclusiveSecondaryVertexHighPurBJetTags',
+                      #,'combinedInclusiveSecondaryVertexBJetTags','doubleSecondaryVertexHighEffBJetTags']
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PAT")
+
+## MessageLogger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+############## IMPORTANT ########################################
+# If you run over many samples and you save the log, remember to reduce
+# the size of the output by prescaling the report of the event number
+process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
+process.MessageLogger.cerr.default.limit = 10
+#################################################################
+
+## Geometry and Detector Conditions
+process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = globalTag + '::All'
+
+## Events to process
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+
+## Options and Output Report
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(options.wantSummary) )
+
+#-------------------------------------
+## Input files
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        'file:patTuple_PF2PAT_mc.root'
+    )
+)
+
+#-------------------------------------
+outFilename = string.replace(options.outFilename,'.root','') + '_mc.root'
+if options.runOnData :
+    outFilename = string.replace(options.outFilename,'.root','') + '_data.root'
+
+## Output file
+process.TFileService = cms.Service("TFileService",
+   fileName = cms.string(outFilename)
+)
+#-------------------------------------
+## Standard PAT Configuration File
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+## Output Module Configuration (expects a path 'p')
+from PhysicsTools.PatAlgos.patEventContent_cff import patEventContent
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string("test.root"),
+    # save only events passing the full path
+    SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
+    # save PAT Layer 1 output; you need a '*' to
+    # unpack the list of commands 'patEventContent'
+    outputCommands = cms.untracked.vstring('drop *', *patEventContent)
+)
+
+#-------------------------------------
+## CA8 jets (Gen and Reco)
+from RecoJets.JetProducers.ca4GenJets_cfi import ca4GenJets
+process.ca8GenJetsNoNu = ca4GenJets.clone(
+    rParam = cms.double(0.8),
+    src = cms.InputTag("genParticlesForJetsNoNu")
+)
+from RecoJets.JetProducers.ca4PFJets_cfi import ca4PFJets
+process.ca8PFJets = ca4PFJets.clone(
+    rParam = cms.double(0.8),
+    src = cms.InputTag("pfNoElectronPFlow"),
+    srcPVs = cms.InputTag("goodOfflinePrimaryVertices"),
+    doAreaFastjet = cms.bool(True)
+)
+
+## CA8 filtered jets (Gen and Reco) (each module produces two jet collections, fat jets and subjets)
+from RecoJets.JetProducers.ca4GenJets_cfi import ca4GenJets
+process.ca8GenJetsNoNuFiltered = ca4GenJets.clone(
+    rParam = cms.double(0.8),
+    src = cms.InputTag("genParticlesForJetsNoNu"),
+    useFiltering = cms.bool(True),
+    nFilt = cms.int32(3),
+    rFilt = cms.double(0.3),
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
+)
+from RecoJets.JetProducers.ak5PFJetsFiltered_cfi import ak5PFJetsFiltered
+process.ca8PFJetsFiltered = ak5PFJetsFiltered.clone(
+    jetAlgorithm = cms.string("CambridgeAachen"),
+    rParam = cms.double(0.8),
+    src = process.ca8PFJets.src,
+    srcPVs = process.ca8PFJets.srcPVs,
+    doAreaFastjet = process.ca8PFJets.doAreaFastjet,
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
+)
+## CA8 pruned jets (Gen and Reco) (each module produces two jet collections, fat jets and subjets)
+from RecoJets.JetProducers.SubJetParameters_cfi import SubJetParameters
+process.ca8GenJetsNoNuPruned = ca4GenJets.clone(
+    SubJetParameters,
+    rParam = cms.double(0.8),
+    src = cms.InputTag("genParticlesForJetsNoNu"),
+    usePruning = cms.bool(True),
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
+)
+from RecoJets.JetProducers.ak5PFJetsPruned_cfi import ak5PFJetsPruned
+process.ca8PFJetsPruned = ak5PFJetsPruned.clone(
+    jetAlgorithm = cms.string("CambridgeAachen"),
+    rParam = cms.double(0.8),
+    src = process.ca8PFJets.src,
+    srcPVs = process.ca8PFJets.srcPVs,
+    doAreaFastjet = process.ca8PFJets.doAreaFastjet,
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
+)
+
+#-------------------------------------
+## PATify the above jets
+from PhysicsTools.PatAlgos.tools.jetTools import *
+## CA8 jets
+switchJetCollection(process,
+    cms.InputTag('ca8PFJets'),
+    doJTA=options.doJTA,
+    doBTagging=options.doBTagging,
+    btagInfo=bTagInfos,
+    btagdiscriminators=bTagDiscriminators,
+    jetCorrLabel = inputJetCorrLabelAK5,
+    doType1MET   = False,
+    genJetCollection = cms.InputTag("ca8GenJetsNoNu"),
+    doJetID      = False,
+)
+## Filtered CA8 jets
+addJetCollection(
+    process,
+    cms.InputTag('ca8PFJetsFiltered'),
+    'CA8Filtered','PF',
+    doJTA=False,
+    doBTagging=False,
+    btagInfo=bTagInfos,
+    btagdiscriminators=bTagDiscriminators,
+    jetCorrLabel=inputJetCorrLabelAK7,
+    doType1MET=False,
+    doL1Cleaning=False,
+    doL1Counters=False,
+    doJetID=False,
+    genJetCollection=cms.InputTag("ca8GenJetsNoNu")
+)
+## Filtered subjets of CA8 jets
+addJetCollection(
+    process,
+    cms.InputTag('ca8PFJetsFiltered','SubJets'),
+    'CA8FilteredSubjets', 'PF',
+    doJTA=options.doJTA,
+    doBTagging=options.doBTagging,
+    btagInfo=bTagInfos,
+    btagdiscriminators=bTagDiscriminators,
+    jetCorrLabel=inputJetCorrLabelAK5,
+    doType1MET=False,
+    doL1Cleaning=False,
+    doL1Counters=False,
+    doJetID=False,
+    genJetCollection=cms.InputTag('ca8GenJetsNoNuFiltered','SubJets')
+)
+## Pruned CA8 jets
+addJetCollection(
+    process,
+    cms.InputTag('ca8PFJetsPruned'),
+    'CA8Pruned','PF',
+    doJTA=False,
+    doBTagging=False,
+    btagInfo=bTagInfos,
+    btagdiscriminators=bTagDiscriminators,
+    jetCorrLabel=inputJetCorrLabelAK7,
+    doType1MET=False,
+    doL1Cleaning=False,
+    doL1Counters=False,
+    doJetID=False,
+    genJetCollection=cms.InputTag("ca8GenJetsNoNu")
+)
+## Pruned subjets of CA8 jets
+addJetCollection(
+    process,
+    cms.InputTag('ca8PFJetsPruned','SubJets'),
+    'CA8PrunedSubjets', 'PF',
+    doJTA=options.doJTA,
+    doBTagging=options.doBTagging,
+    btagInfo=bTagInfos,
+    btagdiscriminators=bTagDiscriminators,
+    jetCorrLabel=inputJetCorrLabelAK5,
+    doType1MET=False,
+    doL1Cleaning=False,
+    doL1Counters=False,
+    doJetID=False,
+    genJetCollection=cms.InputTag('ca8GenJetsNoNuPruned','SubJets')
+)
+
+#-------------------------------------
+from PhysicsTools.PatAlgos.tools.coreTools import *
+## Keep only jets in the default sequence
+removeAllPATObjectsBut(process, ['Jets'])
+
+## Remove MC matching when running over data
+if options.runOnData:
+    removeMCMatching( process, ['All'] )
+
+#-------------------------------------
+## GenParticles for GenJets
+from RecoJets.Configuration.GenJetParticles_cff import genParticlesForJetsNoNu
+process.genParticlesForJetsNoNu = genParticlesForJetsNoNu
+
+#-------------------------------------
+## Produce a collection of good primary vertices
+from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
+process.goodOfflinePrimaryVertices = cms.EDFilter("PrimaryVertexObjectFilter",
+    filterParams = pvSelector.clone(
+        minNdof = cms.double(4.0), # this is >= 4
+        maxZ = cms.double(24.0),
+        maxRho = cms.double(2.0)
+    ),
+    src = cms.InputTag('offlinePrimaryVertices')
+)
+
+## Good primary vertex event filter
+process.primaryVertexFilter = cms.EDFilter('VertexSelector',
+    src = cms.InputTag('offlinePrimaryVertices'),
+    cut = cms.string('!isFake & ndof > 4 & abs(z) <= 24 & position.Rho <= 2'),
+    filter = cms.bool(True)
+)
+
+#-------------------------------------
+## Define your analyzer and/or ntuple maker
+#process.myAnalyzer = ...
+
+#process.myNtupleMaker = ...
+
+#-------------------------------------
+## If using explicit jet-track association
+#if options.useExplicitJTA:
+    #from RecoJets.JetAssociationProducers.ak5JTA_cff import ak5JetTracksAssociatorExplicit
+    #for m in getattr(process,"patDefaultSequence").moduleNames():
+        #if m.startswith('jetTracksAssociatorAtVertex'):
+            #print 'Switching ' + m + ' to explicit jet-track association'
+            #setattr( process, m, ak5JetTracksAssociatorExplicit.clone(jets = getattr(getattr(process,m),'jets')) )
+
+#-------------------------------------
+## Remove tau stuff that really shouldn't be there (probably a bug in PAT)
+process.patDefaultSequence.remove(process.kt6PFJetsForRhoComputationVoronoi)
+for m in getattr(process,"patDefaultSequence").moduleNames():
+    if m.startswith('hpsPFTau'):
+        getattr(process,"patDefaultSequence").remove(getattr(process,m))
+
+#-------------------------------------
+## Define jet sequences
+process.genJetSeq = cms.Sequence(
+    process.ca8GenJetsNoNu
+    + process.ca8GenJetsNoNuFiltered
+    + process.ca8GenJetsNoNuPruned
+)
+process.jetSeq = cms.Sequence(
+    process.ca8PFJets
+    + process.ca8PFJetsFiltered
+    + process.ca8PFJetsPruned
+)
+
+if not options.runOnData:
+    process.jetSeq = cms.Sequence( process.genParticlesForJetsNoNu * process.genJetSeq + process.jetSeq )
+
+#-------------------------------------
+## Path definition
+process.p = cms.Path(
+    process.primaryVertexFilter
+    * process.goodOfflinePrimaryVertices
+    * ( process.jetSeq * process.patDefaultSequence )
+    #* (
+    #process.myAnalyzer
+    #+ process.myNtupleMaker
+    #)
+)
+
+## Delete output module
+del process.out
+
+## Schedule definition
+process.schedule = cms.Schedule(process.p)
